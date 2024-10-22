7a2999f299238c0d893eb66be717c3527d358350
Connor <connormoorebusiness@gmail.com>
2024-10-22 12:04:39 -0500

    Fixed play command to handle both playlist and search query

--- mlean/Commands/Play.cs
+++ mlean/Commands/Play.cs
@@ -4,97 +4,105 @@ using Discord.WebSocket;
 using Lavalink4NET.Players;
 using Lavalink4NET.Rest.Entities.Tracks;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace mlean.Commands
 {
-    public class Play(IAudioService audioService, DiscordSocketClient discordClient)
-        : CommandBase(audioService)
+    public class Play : CommandBase
     {
+        private readonly DiscordSocketClient _discordClient;
+
+        public Play(IAudioService audioService, DiscordSocketClient discordClient) : base(audioService)
+        {
+            _discordClient = discordClient;
+        }
 
         [Command("play", RunMode = RunMode.Async)]
-public async Task PlayAsync([Remainder] string searchQuery)
-{
-    if (string.IsNullOrWhiteSpace(searchQuery))
-    {
-        await ReplyAsync(embed: Utilities.ErrorEmbed("Provide a search query."));
-        return;
-    }
-
-    var tracks = await AudioService.Tracks.LoadTracksAsync(searchQuery, TrackSearchMode.YouTube);
-    if (!tracks.HasMatches)
-    {
-        await ReplyAsync(embed: Utilities.ErrorEmbed($"No results for `{searchQuery}`."));
-        return;
-    }
-
-    var player = await GetPlayerAsync(true);
-    AudioManager.Initialize(AudioService, Context, discordClient);
-
-    if (player == null) return;
-
-    // Check if it's a playlist based on track count
-    if (tracks.Tracks.Length > 1)
-    {
-        var firstTrack = tracks.Tracks.First(x => !x.IsLiveStream && x.IsSeekable);
-
-        // If player is not playing, start playing the first track
-        if (player.State != PlayerState.Playing)
+        public async Task PlayAsync([Remainder] string searchQuery)
         {
-            await player.PlayAsync(firstTrack);
-
-            // Set volume if it's not set already
-            if (!AudioManager.Volume)
+            if (string.IsNullOrWhiteSpace(searchQuery))
             {
-                await player.SetVolumeAsync(0.1f);
-                AudioManager.Volume = true;
+                await ReplyAsync(embed: Utilities.ErrorEmbed("Provide a search query."));
+                return;
             }
 
-            AudioManager.UpdateTrackEvent();
+            // Check if the search query is a YouTube playlist URL
+            bool isPlaylist = Regex.IsMatch(searchQuery, @"(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/.*(?:list=)([a-zA-Z0-9_-]+)");
 
-            // Enqueue the rest of the tracks
-            foreach (var track in tracks.Tracks.Skip(1).Where(x => !x.IsLiveStream && x.IsSeekable))
+            var trackSearchMode = isPlaylist ? TrackSearchMode.YouTube : TrackSearchMode.YouTube; // Use YouTube search mode in both cases
+            var tracks = await AudioService.Tracks.LoadTracksAsync(searchQuery, trackSearchMode);
+
+            if (!tracks.HasMatches)
             {
-                await player.Queue.AddAsync(new TrackQueueItem(track));
+                await ReplyAsync(embed: Utilities.ErrorEmbed($"No results for `{searchQuery}`."));
+                return;
             }
 
-            await ReplyAsync(embed: Utilities.CreatePlaylistEmbed(tracks.Tracks.Length, "Started playing playlist and added to queue"));
-        }
-        else
-        {
-            // If player is already playing, add all tracks to the queue
-            foreach (var track in tracks.Tracks.Where(x => !x.IsLiveStream && x.IsSeekable))
+            var player = await GetPlayerAsync(true);
+            AudioManager.Initialize(AudioService, Context, _discordClient);
+
+            if (player == null) return;
+
+            // If it's a YouTube playlist URL, handle it as a playlist
+            if (isPlaylist && tracks.Tracks.Length > 1)
             {
-                await player.Queue.AddAsync(new TrackQueueItem(track));
+                var firstTrack = tracks.Tracks.First(x => !x.IsLiveStream && x.IsSeekable);
+
+                if (player.State != PlayerState.Playing)
+                {
+                    await player.PlayAsync(firstTrack);
+
+                    if (!AudioManager.Volume)
+                    {
+                        await player.SetVolumeAsync(0.1f);
+                        AudioManager.Volume = true;
+                    }
+
+                    AudioManager.UpdateTrackEvent();
+
+                    // Enqueue the rest of the tracks
+                    foreach (var track in tracks.Tracks.Skip(1).Where(x => !x.IsLiveStream && x.IsSeekable))
+                    {
+                        await player.Queue.AddAsync(new TrackQueueItem(track));
+                    }
+
+                    await ReplyAsync(embed: Utilities.CreatePlaylistEmbed(tracks.Tracks.Length, "Started playing playlist and added to queue"));
+                }
+                else
+                {
+                    // If player is already playing, add all tracks to the queue
+                    foreach (var track in tracks.Tracks.Where(x => !x.IsLiveStream && x.IsSeekable))
+                    {
+                        await player.Queue.AddAsync(new TrackQueueItem(track));
+                    }
+
+                    await ReplyAsync(embed: Utilities.CreatePlaylistEmbed(tracks.Tracks.Length, "Playlist added to queue"));
+                }
             }
-
-            await ReplyAsync(embed: Utilities.CreatePlaylistEmbed(tracks.Tracks.Length, "Playlist added to queue"));
-        }
-    }
-    else
-    {
-        // Single track case
-        var track = tracks.Tracks.First(x => !x.IsLiveStream && x.IsSeekable);
-
-        if (player.State == PlayerState.Playing)
-        {
-            await player.Queue.AddAsync(new TrackQueueItem(track));
-            await ReplyAsync(embed: Utilities.CreateTrackEmbed(track, "Added to Queue"));
-        }
-        else
-        {
-            await player.PlayAsync(track);
-
-            // Set volume if it's not set already
-            if (!AudioManager.Volume)
+            else
             {
-                await player.SetVolumeAsync(0.1f);
-                AudioManager.Volume = true;
+                // Single track case
+                var track = tracks.Tracks.First(x => !x.IsLiveStream && x.IsSeekable);
+
+                if (player.State == PlayerState.Playing)
+                {
+                    await player.Queue.AddAsync(new TrackQueueItem(track));
+                    await ReplyAsync(embed: Utilities.CreateTrackEmbed(track, "Added to Queue"));
+                }
+                else
+                {
+                    await player.PlayAsync(track);
+
+                    if (!AudioManager.Volume)
+                    {
+                        await player.SetVolumeAsync(0.1f);
+                        AudioManager.Volume = true;
+                    }
+
+                    AudioManager.UpdateTrackEvent();
+                }
             }
-
-            AudioManager.UpdateTrackEvent();
         }
-    }
-}
-
     }
 }
